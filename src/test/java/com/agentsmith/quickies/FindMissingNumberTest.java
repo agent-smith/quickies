f3024b4d0f8af539b54526079893d178c1b1d9b8
agent-smith <randy31415@gmail.com>
2013-07-30 22:41:55 -0500

    prog examples for finding missing number in an array of integers from 1..N

--- src/test/java/com/agentsmith/quickies/FindMissingNumberTest.java
+++ src/test/java/com/agentsmith/quickies/FindMissingNumberTest.java
@@ -44,4 +44,16 @@ public class FindMissingNumberTest
         int actualMissingNum = f.findMissingNum(new Integer[] { 1, 2, 4, 5 });
         assertEquals(3, actualMissingNum);
     }
+
+    @Test
+    public void findMissing13In50() throws Exception
+    {
+        FindMissingNumber f = new FindMissingNumber();
+        int actualMissingNum = f.findMissingNum(new Integer[] {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, // 13 is missing
+                14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+                39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50
+        });
+        assertEquals(13, actualMissingNum);
+    }
 }
