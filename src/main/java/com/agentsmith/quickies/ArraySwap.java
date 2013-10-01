9e14933f55ec30f98dbb9b1f4fc14c9b6e0b13f5
agent-smith <randy31415@gmail.com>
2013-09-30 22:22:57 -0500

    Use ArraySwap utility in ReverseString

--- src/main/java/com/agentsmith/quickies/ArraySwap.java
+++ src/main/java/com/agentsmith/quickies/ArraySwap.java
@@ -5,29 +5,46 @@ import java.util.Arrays;
 import static java.lang.String.format;
 
 /**
- * // TODO: impl me!
+ * Swaping to elements of an array.
  * <p/>
  * User: rmarquez
  * Date: 9/28/13
  * Time: 7:23 PM
  */
-public class ArraySwap
-{
-    public static <T> void swap(T[] arr, int a, int b)
-    {
+public class ArraySwap {
+
+    public static <T> void swap(T[] arr, int a, int b) {
         T temp = arr[a];
         arr[a] = arr[b];
         arr[b] = temp;
     }
 
-    public static void main(String[] args)
-    {
-        Character[] charArray = new Character[] { 'a', 'b', 'c' };
+    public static void swap(char[] arr, int a, int b) {
+        char temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 
-        System.out.println(format("Array of characters: %s", Arrays.toString(charArray)));
+    public static void swap(int[] arr, int a, int b) {
+        arr[a] = arr[a]^arr[b];
+        arr[b] = arr[a]^arr[b];
+        arr[a] = arr[a]^arr[b];
+    }
 
-        ArraySwap.swap(charArray, 0, 2);
+    public static void main(String[] args) {
+        Character[] characters = new Character[] { 'a', 'b', 'c' };
+        System.out.println(format("Array of Characters: %s", Arrays.toString(characters)));
+        ArraySwap.swap(characters, 0, 2);
+        System.out.println(format("Swapping items '%d' and '%d' gives: %s", 0, 2, Arrays.toString(characters)));
 
-        System.out.println(format("Swapping items '%d' and '%d' gives: %s", 0, 2, Arrays.toString(charArray)));
+        char[] chars = new char[] { 'a', 'b', 'c' };
+        System.out.println(format("\nArray of chars: %s", Arrays.toString(chars)));
+        ArraySwap.swap(chars, 0, 2);
+        System.out.println(format("Swapping items '%d' and '%d' gives: %s", 0, 2, Arrays.toString(chars)));
+
+        int[] ints = new int[] { 1, 2, 3 };
+        System.out.println(format("\nArray of ints: %s", Arrays.toString(ints)));
+        ArraySwap.swap(ints, 0, 2);
+        System.out.println(format("Swapping items '%d' and '%d' gives: %s", 0, 2, Arrays.toString(ints)));
     }
 }
--- src/main/java/com/agentsmith/quickies/ReverseString.java
+++ src/main/java/com/agentsmith/quickies/ReverseString.java
@@ -1,28 +1,24 @@
 package com.agentsmith.quickies;
 
-public class ReverseString
-{
-    public String reverse(String strToReverse)
-    {
+public class ReverseString {
+
+    public String reverse(String strToReverse) {
         char[] chars = strToReverse.toCharArray();
-        for (int i = 0; i < chars.length / 2; i++)
-        {
-            swap(chars, i, (chars.length - 1) - i);
+        for (int i = 0; i < chars.length / 2; i++) {
+            ArraySwap.swap(chars, i, (chars.length - 1) - i);
         }
 
         return String.valueOf(chars);
     }
 
-    private void swap(char[] chars, int a, int b)
-    {
-        char temp = chars[a];
-        chars[a] = chars[b];
-        chars[b] = temp;
-    }
+    public static void main(String[] args) {
 
-    public static void main(String[] args)
-    {
-        String strToReverse = args[0];
+        String strToReverse;
+        if (args.length != 1) {
+            strToReverse = "123456";
+        } else {
+            strToReverse = args[0];
+        }
 
         System.out.println("Reversing following String: " + strToReverse);
 
