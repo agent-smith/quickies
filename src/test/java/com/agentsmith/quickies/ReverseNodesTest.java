7ba5d24a67eb47b1407a039eb5541bab3bf7ddaa
agent-smith <randy31415@gmail.com>
2014-02-01 13:38:08 -0600

    fix remove

--- src/test/java/com/agentsmith/quickies/ReverseNodesTest.java
+++ src/test/java/com/agentsmith/quickies/ReverseNodesTest.java
@@ -14,53 +14,97 @@ import static org.junit.Assert.assertEquals;
  */
 public class ReverseNodesTest
 {
+    // Recursive tests
+
     @Test
-    public void reverseNullNode()
+    public void reverseRecursive_NullNode()
     {
         Node<String> node = null;
 
-        ReverseNodes rll = new ReverseNodes();
-        Node<String> actual = rll.reverse(node);
+        Node<String> actual = ReverseNodes.reverseRecursive(node);
         assertNull(actual);
     }
 
     @Test
-    public void reverseOneNode()
+    public void reverseRecursive_OneNode()
     {
         Node<String> node = new Node<>("A", null);
 
-        ReverseNodes rll = new ReverseNodes();
-        Node<String> actual = rll.reverse(node);
+        Node<String> actual = ReverseNodes.reverseRecursive(node);
         assertEquals(new Node<>("A", null), actual);
     }
 
     @Test
-    public void reverseTwoNodes()
+    public void reverseRecursive_TwoNodes()
     {
         Node<String> node = new Node<>("A", new Node<>("B", null));
 
-        ReverseNodes rll = new ReverseNodes();
-        Node<String> actual = rll.reverse(node);
+        Node<String> actual = ReverseNodes.reverseRecursive(node);
         assertEquals(new Node<>("B", new Node<>("A", null)), actual);
     }
 
     @Test
-    public void reverseThreeNodes()
+    public void reverseRecursive_ThreeNodes()
     {
         Node<String> node = new Node<>("A", new Node<>("B", new Node<>("C", null)));
 
-        ReverseNodes rll = new ReverseNodes();
-        Node<String> actual = rll.reverse(node);
+        Node<String> actual = ReverseNodes.reverseRecursive(node);
         assertEquals(new Node<>("C", new Node<>("B", new Node<>("A", null))), actual);
     }
 
     @Test
-    public void reverseFourNodes()
+    public void reverseRecursive_FourNodes()
     {
         Node<String> node = new Node<>("A", new Node<>("B", new Node<>("C", new Node<>("D", null))));
 
-        ReverseNodes rll = new ReverseNodes();
-        Node<String> actual = rll.reverse(node);
+        Node<String> actual = ReverseNodes.reverseRecursive(node);
+        assertEquals(new Node<>("D", new Node<>("C", new Node<>("B", new Node<>("A", null)))), actual);
+    }
+
+    // Iterative tests
+
+    @Test
+    public void reverseIterative_NullNode()
+    {
+        Node<String> node = null;
+
+        Node<String> actual = ReverseNodes.reverseIterative(node);
+        assertNull(actual);
+    }
+
+    @Test
+    public void reverseIterative_OneNode()
+    {
+        Node<String> node = new Node<>("A", null);
+
+        Node<String> actual = ReverseNodes.reverseIterative(node);
+        assertEquals(new Node<>("A", null), actual);
+    }
+
+    @Test
+    public void reverseIterative_TwoNodes()
+    {
+        Node<String> node = new Node<>("A", new Node<>("B", null));
+
+        Node<String> actual = ReverseNodes.reverseIterative(node);
+        assertEquals(new Node<>("B", new Node<>("A", null)), actual);
+    }
+
+    @Test
+    public void reverseIterative_ThreeNodes()
+    {
+        Node<String> node = new Node<>("A", new Node<>("B", new Node<>("C", null)));
+
+        Node<String> actual = ReverseNodes.reverseIterative(node);
+        assertEquals(new Node<>("C", new Node<>("B", new Node<>("A", null))), actual);
+    }
+
+    @Test
+    public void reverseIterative_FourNodes()
+    {
+        Node<String> node = new Node<>("A", new Node<>("B", new Node<>("C", new Node<>("D", null))));
+
+        Node<String> actual = ReverseNodes.reverseIterative(node);
         assertEquals(new Node<>("D", new Node<>("C", new Node<>("B", new Node<>("A", null)))), actual);
     }
 }
